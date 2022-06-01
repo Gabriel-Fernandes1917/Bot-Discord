2081ad686482d4b839245085a08fd74c15534c2c
Gabriel Fernandes <gabgui2001@gmail.com>
2022-06-01 17:18:57 -0300

    add comandos das questoes

--- index.js
+++ index.js
@@ -16,23 +16,50 @@ const client = new Client({
 
 client.once('ready', function(){
     console.log("bote ON ")
+}, function(message){
+    
+    message.reply("Salve clã, estou on novamente pra ajudar vcs ! <3 \n")
+    //não ta mandnado mensagem
 })
 
 client.on('messageCreate', function(message){
-    if (message.content == "oi") {
-        message.reply("oi krl tudo bem ? ")
-    }
-    if (message.content == "o gerarde vai fazer uma festa na mansão dele ?") {
-        message.reply("Claro po, resenha de 50% so se tem 1 vez")
-    }
-
-    if (message.content == "Quem e voce ?") {
-        message.reply("ola sou o 1 bot 100% programado a mão pelo @GeGi#9490")
+    if (message.content == ";help") {
+        message.reply("Ola, sou o bot da Rama T.I, esses são os meu comandos: ")
+        message.reply("1- Questionarios de perguntas\ 2 - boas vindas \n")
+        message.reply("Estou as suas ordens, para chamar um comando digite ; e o numero do comando ex ;1 , abraço Rama T.I ")
     }
 
-    if (message.content == ".play") {
-        message.reply("Espera porra, ainda to começando nessa vida de DJ, jaja essa função vai tar on... Aguarde")
+    // comandos do questionario
+    let respsota;
+    let acertos = 0
+
+    if (message.content == ";1") {
+        message.reply("OK, vamos la vou lhe fazer 10 perguntas se vc acertar 70% voce ganha, para responder digite ;letra correta e o numero da questão ex ;A1 . preparado ? la vamos nos")
+
+        //q1()
     }
+
+    function q1(){
+        // console.log("entrei funcao")
+        message.reply("Qual das seguintes respostas descreve uma condição de teste? \n a) Uma característica distinta de um componente ou sistema. \n b) Um aspecto testável de um componente ou sistema identificado como base para os testes. \n c) O grau em que um produto de software fornece funço es que atendem a s necessidades declaradas e implícitas quando o software e utilizado sob condições específicas. \n d) Casos de teste projetados para executar combinações de condições ações resultantes delas.");
+        setTimeout(() => {  
+            respsota = message.content
+            if(respsota == ";A1"){
+                message.reply("certa resposta")
+                acertos++
+            }else{
+                message.reply("errada a resposta")
+            }
+         }, 10000);
+        
+    }
+
+    function comando1() {
+        message.reply("")
+    }
+
+    //fim questionario 
+   
     
 })
 
