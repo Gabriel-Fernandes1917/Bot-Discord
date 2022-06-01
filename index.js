a94360f621e20463313ae0ff2ee7661aa894192c
Gabriel Fernandes <gabgui2001@gmail.com>
2022-06-01 20:32:38 -0300

    ,

--- index.js
+++ index.js
@@ -23,42 +23,47 @@ client.once('ready', function(){
 })
 
 client.on('messageCreate', function(message){
-    if (message.content == ";help") {
-        message.reply("Ola, sou o bot da Rama T.I, esses são os meu comandos: ")
-        message.reply("1- Questionarios de perguntas\ 2 - boas vindas \n")
-        message.reply("Estou as suas ordens, para chamar um comando digite ; e o numero do comando ex ;1 , abraço Rama T.I ")
-    }
 
-    // comandos do questionario
-    let respsota;
-    let acertos = 0
-
-    if (message.content == ";1") {
-        message.reply("OK, vamos la vou lhe fazer 10 perguntas se vc acertar 70% voce ganha, para responder digite ;letra correta e o numero da questão ex ;A1 . preparado ? la vamos nos")
-
-        //q1()
-    }
-
-    function q1(){
-        // console.log("entrei funcao")
-        message.reply("Qual das seguintes respostas descreve uma condição de teste? \n a) Uma característica distinta de um componente ou sistema. \n b) Um aspecto testável de um componente ou sistema identificado como base para os testes. \n c) O grau em que um produto de software fornece funço es que atendem a s necessidades declaradas e implícitas quando o software e utilizado sob condições específicas. \n d) Casos de teste projetados para executar combinações de condições ações resultantes delas.");
-        setTimeout(() => {  
-            respsota = message.content
-            if(respsota == ";A1"){
-                message.reply("certa resposta")
-                acertos++
-            }else{
-                message.reply("errada a resposta")
-            }
-         }, 10000);
+    if (message.content[0] == "!") {
         
+        if (message.content == "!help") {
+            message.reply("Ola, sou o bot da Rama T.I, esses são os meu comandos: \n 1- Questionarios de perguntas\ 2 - boas vindas \n Estou as suas ordens, para chamar um comando digite ! e o numero do comando ex !1 , abraço Rama T.I")
+        }
+    
+        // comandos do questionario
+        let respsota;
+        let acertos = 0
+    
+        if (message.content == "!1") {
+            message.reply("OK, vamos la vou lhe fazer 10 perguntas se vc acertar 70% voce ganha, para responder digite ;letra correta e o numero da questão ex ;A1 . preparado ? la vamos nos")
+                
+            //q1()
+        }
+
+        
+    
+        function q1(){
+            // console.log("entrei funcao")
+            message.reply("Qual das seguintes respostas descreve uma condição de teste? \n a) Uma característica distinta de um componente ou sistema. \n b) Um aspecto testável de um componente ou sistema identificado como base para os testes. \n c) O grau em que um produto de software fornece funço es que atendem a s necessidades declaradas e implícitas quando o software e utilizado sob condições específicas. \n d) Casos de teste projetados para executar combinações de condições ações resultantes delas.");
+            setTimeout(() => {  
+                respsota = message.content
+                if(respsota == ";A1"){
+                    message.reply("certa resposta")
+                    acertos++
+                }else{
+                    message.reply("errada a resposta")
+                }
+             }, 10000);
+            
+        }
+    
+        function comando1() {
+            message.reply("")
+        }
+    
+        //fim questionario 
     }
 
-    function comando1() {
-        message.reply("")
-    }
-
-    //fim questionario 
    
     
 })
